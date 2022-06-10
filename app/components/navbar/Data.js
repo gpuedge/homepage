94752ceb2145864281652b8e73f862563b303e5d
vans163 <vans_163@yahoo.com>
2022-06-10 07:29:05 -0400

    add first arm64 node

--- app/components/navbar/Data.js
+++ app/components/navbar/Data.js
@@ -1,11 +1,19 @@
 const Data = [
   {
-    id: "1",
+    id: "0",
     title: "node0.gpux.ai",
     url: "https://node0.gpux.ai/",
     category: "Online",
     //price: "(CPU: 40/40) (RAM: 128/128G) (GPU: 4/4)",
     price: "(Bench: 20678 | CPU: 40 | RAM: 128G | GPU: 4)",
+  },
+  {
+    id: "1",
+    title: "arm0.gpux.ai",
+    url: "https://arm0.gpux.ai/",
+    category: "Online",
+    //price: "(CPU: 40/40) (RAM: 128/128G) (GPU: 4/4)",
+    price: "(Bench: ? | AARCH64 | CPU: 32 | RAM: 32G)",
   },
 ];
 
